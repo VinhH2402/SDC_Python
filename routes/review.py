f92be07d45285b644f1f0da2dfce5470b2e09876
VinhH2402 <78180414+VinhH2402@users.noreply.github.com>
2022-06-21 12:06:37 -0700

    add photos to reviews

--- routes/review.py
+++ routes/review.py
@@ -6,26 +6,35 @@ cur = mydb.cursor()
 
 cur.execute('USE reviewAPI')
 
-#query
-findReviews = 'SELECT * FROM review WHERE product_id = %s'
-findPhotos = 'SELECT * FROM photo WHERE review_id = %s'
+# query
+reviewQuery = '''SELECT  
+                review.* , 
+                group_concat(DISTINCT photo.url) as 'photos'
+                FROM review 
+                LEFT JOIN photo ON review.id = photo.review_id
+                WHERE product_id = %s
+                GROUP BY review.id'''
+
 
 def transform(data):
     results = []
     for r in data:
         date = datetime.fromtimestamp(int(r[3])/1000)
+        print(type(r[12]))
+        photo = []
+        if(r[12] is not None):
+            photo = r[12].split(',')
         review = {
-        'review_id': r[1],
-        'rating': r[2],
-        'data': date,
-        'summary': r[4],
-        'body': r[5],
-        'recommend': r[6],
-        'reported': r[7],
-        'reviewer_name': r[8],
-        'response': r[10],
-        'helfulness': r[11],
-        'photo': []
+            'review_id': r[1],
+            'rating': r[2],
+            'date': date,
+            'summary': r[4],
+            'body': r[5],
+            'recommend': r[6],
+            'reviewer_name': r[8],
+            'response': r[10],
+            'helfulness': r[11],
+            'photos': photo
         }
         results.append(review)
     return results
@@ -36,11 +45,16 @@ def getReviews(request):
         product_id = request.args.get('product_id', default=1)
         page = request.args.get('page', default=1)
         count = request.args.get('count', default=5)
-        cur.execute(findReviews, (product_id,))
+        cur.execute(reviewQuery, (product_id,))
         reviews = cur.fetchall()
-    
-        print(product_id, page, count)
+
         data = transform(reviews)
-        return {'results': data}
+
+        return {
+            'product': product_id,
+            'page': page,
+            'count': count,
+            'results': data
+        }
     else:
         return 'post reviews'
